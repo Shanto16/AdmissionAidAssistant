92d9ec05473c2ad8678aa14fc2edcf38aadd7ade
Shanto16 <shakil.glostars@gmail.com>
2019-04-09 00:53:20 +0600

    public varsity profiles

---
+++ app/src/main/java/tanvi/admissionpro/KUETProfile.java
@@ -0,0 +1,13 @@
+package tanvi.admissionpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KUETProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kuetprofile);
+    }
+}
