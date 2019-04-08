6908bc7bad9e00571edefc43f18e83de3622c5d8
Shanto16 <shakil.glostars@gmail.com>
2019-04-09 00:36:11 +0600

    private varsity profiles

---
+++ app/src/main/java/tanvi/admissionpro/BracProfile.java
@@ -0,0 +1,13 @@
+package tanvi.admissionpro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BracProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_brac);
+    }
+}
---
+++ app/src/main/java/tanvi/admissionpro/DUProfile.java
@@ -0,0 +1,13 @@
+package tanvi.admissionpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DUProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_duprofile);
+    }
+}
